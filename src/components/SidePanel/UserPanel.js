8136924db4e6bfbdb7dd4d1ec695f98a2e07bd0e
Ricardo Hui <eehaha@gmail.com>
2019-05-15 08:14:43 +0800

    Scafford App Components

---
+++ src/components/SidePanel/UserPanel.js
@@ -0,0 +1,51 @@
+import React from "react";
+import firebase from "../../firebase";
+import { Grid, Header, Icon, Dropdown } from "semantic-ui-react";
+
+class UserPanel extends React.Component {
+  dropdownOptions = () => [
+    {
+      key: "user",
+      text: (
+        <span>
+          signed in as <strong>User</strong>
+        </span>
+      ),
+      disabled: true
+    },
+    { key: "avatar", text: <span>Change Avatar</span> },
+    { key: "signout", text: <span onClick={this.handleSignOut}>Sign Out</span> }
+  ];
+
+  handleSignOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        console.log("signout");
+      });
+  };
+
+  render() {
+    return (
+      <Grid style={{ background: "#4c3c4c" }}>
+        <Grid.Column>
+          <Grid.Row style={{ padding: "1.2em", margin: 0 }}>
+            <Header inverted floated="left" as="h2">
+              <Icon name="code" />
+              <Header.Content>DevChat</Header.Content>
+            </Header>
+          </Grid.Row>
+          <Header style={{ padding: "0.25em" }} as="h4" inverted>
+            <Dropdown
+              trigger={<span>User</span>}
+              options={this.dropdownOptions()}
+            />
+          </Header>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default UserPanel;
