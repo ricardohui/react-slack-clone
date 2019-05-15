0043f534fffa9e911c75e769d3e5a871481d7c2f
Ricardo Hui <eehaha@gmail.com>
2019-05-16 07:09:19 +0800

    Add Channel Component

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,13 @@ import { Grid } from "semantic-ui-react";
 import ColorPanel from "./ColorPanel/ColorPanel";
 import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
+import { connect } from "react-redux";
 import MetaPanel from "./MetaPanel/MetaPanel";
-const App = () => {
+const App = ({ currentUser }) => {
   return (
     <Grid columns="equal" className="app" style={{ background: "#eee" }}>
       <ColorPanel />
-      <SidePanel />
+      <SidePanel currentUser={currentUser} />
       <Grid.Column style={{ marginLeft: 320 }}>
         <Messages />
       </Grid.Column>
@@ -20,4 +21,6 @@ const App = () => {
   );
 };
 
-export default App;
+const mapStateToProps = state => ({ currentUser: state.user.currentUser });
+
+export default connect(mapStateToProps)(App);
--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,14 +1,17 @@
 import React from "react";
 import firebase from "../../firebase";
-import { Grid, Header, Icon, Dropdown } from "semantic-ui-react";
-
+import { Grid, Header, Icon, Dropdown, Image } from "semantic-ui-react";
 class UserPanel extends React.Component {
+  state = {
+    user: this.props.currentUser
+  };
   dropdownOptions = () => [
     {
       key: "user",
       text: (
         <span>
-          signed in as <strong>User</strong>
+          signed in as
+          <strong>{this.state.user.displayName}</strong>
         </span>
       ),
       disabled: true
@@ -16,6 +19,10 @@ class UserPanel extends React.Component {
     { key: "avatar", text: <span>Change Avatar</span> },
     { key: "signout", text: <span onClick={this.handleSignOut}>Sign Out</span> }
   ];
+
+  componentDidMount() {
+    this.setState({ user: this.props.currentUser });
+  }
 
   handleSignOut = () => {
     firebase
@@ -27,6 +34,7 @@ class UserPanel extends React.Component {
   };
 
   render() {
+    const { user } = this.state;
     return (
       <Grid style={{ background: "#4c3c4c" }}>
         <Grid.Column>
@@ -35,13 +43,18 @@ class UserPanel extends React.Component {
               <Icon name="code" />
               <Header.Content>DevChat</Header.Content>
             </Header>
+            <Header style={{ padding: "0.25em" }} as="h4" inverted>
+              <Dropdown
+                trigger={
+                  <span>
+                    <Image src={user.photoURL} spaced="right" avatar />
+                    {user.displayName}
+                  </span>
+                }
+                options={this.dropdownOptions()}
+              />
+            </Header>
           </Grid.Row>
-          <Header style={{ padding: "0.25em" }} as="h4" inverted>
-            <Dropdown
-              trigger={<span>User</span>}
-              options={this.dropdownOptions()}
-            />
-          </Header>
         </Grid.Column>
       </Grid>
     );
