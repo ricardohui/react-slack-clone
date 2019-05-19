3e6dd8ff4f890bc66c5bb0ee254dd678c5a58a76
Ricardo Hui <eehaha@gmail.com>
2019-05-20 07:38:09 +0800

    Search Messages

--- src/components/Messages/MessageHeader.js
+++ src/components/Messages/MessageHeader.js
@@ -3,18 +3,26 @@ import { Header, Segment, Icon, Input } from "semantic-ui-react";
 
 class MessageHeader extends React.Component {
   render() {
+    const {
+      channelName,
+      numUniqueUsers,
+      handleSearchChange,
+      searchLoading
+    } = this.props;
     return (
       <Segment clearing>
         <Header fluid="true" as="h2" floated="left" style={{ marginButton: 0 }}>
           <span>
-            Channel
+            {channelName}
             <Icon name="star outline" color="black" />
           </span>
-          <Header.Subheader>2 Users</Header.Subheader>
+          <Header.Subheader>{numUniqueUsers}</Header.Subheader>
         </Header>
         <Header floated="right">
           <Input
+            loading={searchLoading}
             size="mini"
+            onChange={handleSearchChange}
             icon="search"
             name="searchTerm"
             placeholder="Search Messages"
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -12,7 +12,11 @@ class Messages extends React.Component {
     channel: this.props.currentChannel,
     user: this.props.currentUser,
     messagesLoading: true,
-    progressBar: false
+    progressBar: false,
+    numUniqueUsers: "",
+    searchTerm: "",
+    searchLoading: false,
+    searchResults: []
   };
 
   componentDidMount() {
@@ -38,7 +42,41 @@ class Messages extends React.Component {
         messages: loadedMessages,
         messagesLoading: false
       });
+      this.countUniqueUsers(loadedMessages);
     });
+  };
+
+  handleSearchChange = event => {
+    this.setState({ searchTerm: event.target.value, searchLoading: true }, () =>
+      this.handleSearchMessages()
+    );
+  };
+
+  handleSearchMessages = () => {
+    const channelMessages = [...this.state.messages];
+    const regex = new RegExp(this.state.searchTerm, "gi");
+    const searchResults = channelMessages.reduce((acc, message) => {
+      if (
+        (message.content && message.content.match(regex)) ||
+        message.user.name.match(regex)
+      ) {
+        acc.push(message);
+      }
+      return acc;
+    }, []);
+    this.setState({ searchResults });
+    setTimeout(() => this.setState({ searchLoading: false }, 1000));
+  };
+  countUniqueUsers = messages => {
+    const uniqueUsers = messages.reduce((acc, message) => {
+      if (!acc.includes(message.user.name)) {
+        acc.push(message.user.name);
+      }
+      return acc;
+    }, []);
+    const plural = uniqueUsers.length > 1 || uniqueUsers.length === 0;
+    const numUniqueUsers = `${uniqueUsers.length} user${plural ? "s" : ""}`;
+    this.setState({ numUniqueUsers });
   };
 
   displayMessages = messages => {
@@ -62,16 +100,34 @@ class Messages extends React.Component {
     }
   };
 
+  displayChannelName = channel => (channel ? `#${channel.name}` : "");
   render() {
-    const { messagesRef, messages, channel, user, progressBar } = this.state;
+    const {
+      messagesRef,
+      messages,
+      searchLoading,
+      channel,
+      user,
+      progressBar,
+      numUniqueUsers,
+      searchTerm,
+      searchResults
+    } = this.state;
     return (
       <React.Fragment>
-        <MessageHeader />
+        <MessageHeader
+          channelName={this.displayChannelName(channel)}
+          numUniqueUsers={numUniqueUsers}
+          handleSearchChange={this.handleSearchChange}
+          searchLoading={searchLoading}
+        />
         <Segment>
           <Comment.Group
             className={ProgressBar ? "message_progress" : "messages"}
           >
-            {this.displayMessages(messages)}
+            {searchTerm
+              ? this.displayMessages(searchResults)
+              : this.displayMessages(messages)}
           </Comment.Group>
         </Segment>
         <MessageForm
